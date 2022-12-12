c4356bb42db289c7512cc94d4617710776960994
tarde <tarde@DESKTOP-VR5PQ8G>
2022-12-12 15:44:27 +0100

    Introduccion proyecto

--- src/Volumen.java
+++ src/Volumen.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Volumen {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Scanner leerradio = new Scanner(System.in);
+		int radio;
+		System.out.println("Introduce el radio de la esfera");
+		radio=leerradio.nextInt();
 
 	}
 
