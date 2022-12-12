7087ead5d9aed0b5dd7a6147e4b7c21ae4e506a6
tarde <tarde@DESKTOP-VR5PQ8G>
2022-12-12 15:54:47 +0100

    Tabulado y Fin de Proyecto

--- src/Volumen.java
+++ src/Volumen.java
@@ -9,24 +9,23 @@ public class Volumen {
 		Scanner leerradio = new Scanner(System.in);
 		int radio;
 		System.out.println("Introduce el radio de la esfera");
-		radio=leerradio.nextInt();
+		radio = leerradio.nextInt();
 		double volumen;
-		volumen = (4 * (Math.PI) * (Math.pow(radio,3))/3);
-		System.out.println("El volumen de la esfera es "+volumen);
+		volumen = (4 * (Math.PI) * (Math.pow(radio, 3)) / 3);
+		System.out.println("El volumen de la esfera es " + volumen);
 		System.out.println("Finalizado Version 1");
-		
+
 		double area;
 		area = (Math.PI) * (Math.pow(radio, 2));
-		System.out.println("El área de la esfera es "+area);
+		System.out.println("El área de la esfera es " + area);
 		System.out.println("Finalizado Version 2");
-		
+
 		double longitud;
-		longitud = 2 * (Math.PI)* radio;
-		System.out.println("La longitud de la circunferencia es "+longitud);
+		longitud = 2 * (Math.PI) * radio;
+		System.out.println("La longitud de la circunferencia es " + longitud);
 		System.out.println("Finalizado Version 3");
 		System.out.println("");
 		System.out.println("Version definitiva y refundida");
-
 
 	}
 
